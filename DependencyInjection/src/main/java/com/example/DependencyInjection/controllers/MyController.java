97b92f1595a25b197b76078ef5e25a42148a9126
Fahim Makhani <fahimmakhanifm@gmail.com>
2022-11-16 06:01:26 +0530

    DependencyInjection
    --

---
+++ DependencyInjection/src/main/java/com/example/DependencyInjection/controllers/MyController.java
@@ -0,0 +1,14 @@
+package com.example.DependencyInjection.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello World");
+
+        return "Hi Fahim";
+    }
+
+}
